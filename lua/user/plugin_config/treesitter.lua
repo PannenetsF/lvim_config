84f05c4d44066c85673d496fd8b6c2519e39a968
Pannenets F <pannenets.f@foxmail.com>
2023-10-15 23:03:51 +0800

    fix(tree-sitter): ensure sync install

--- lua/user/plugin_config/treesitter.lua
+++ lua/user/plugin_config/treesitter.lua
@@ -2,4 +2,6 @@
 lvim.builtin.treesitter.ensure_installed = { "python", "cpp", "c", "cuda", "yaml", "json", "bash" }
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.auto_install = true
+lvim.builtin.treesitter.sync_install = true
 
